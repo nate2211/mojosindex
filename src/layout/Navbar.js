8a64ae72785cc9d32ee08967c80113956c2e6f77
nate <nate.mcdonald@att.net>
2023-01-24 18:04:39 -0600

    hero

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -7,24 +7,22 @@ import {useErrorContext} from "../context/ErrorContext";
 export function NavBar(){
 
     return(
-        <>
-            <Grid h='10vh' templateColumns='repeat(5, 1fr)' gap={0} position='relative' zIndex={4}>
-                <GridItem colSpan={2} h='100%' bg='gray.900'>
-                    <Center h='100%'>
-                        <SideNav/>
-                    </Center>
-                </GridItem>
-                <GridItem colSpan={1} h='100%' bg='gray.900'>
-                    <Center h='100%'>
-                        <Link to={"/"}><Heading color='whitesmoke'>Mojos Index</Heading></Link>
-                    </Center>
-                </GridItem>
-                <GridItem colStart={4} colEnd={6} h='100%' bg='gray.900'>
-                    <Center h='100%'>
-                        <SideCart/>
-                    </Center>
-                </GridItem>
-            </Grid>
-        </>
+        <Grid h='10vh' templateColumns='repeat(5, 1fr)' gap={0} position='relative' zIndex={4}>
+            <GridItem colSpan={2} h='100%' bg='gray.900'>
+                <Center h='100%'>
+                    <SideNav/>
+                </Center>
+            </GridItem>
+            <GridItem colSpan={1} h='100%' bg='gray.900'>
+                <Center h='100%'>
+                    <Link to={"/"}><Heading color='whitesmoke'>Mojos Index</Heading></Link>
+                </Center>
+            </GridItem>
+            <GridItem colStart={4} colEnd={6} h='100%' bg='gray.900'>
+                <Center h='100%'>
+                    <SideCart/>
+                </Center>
+            </GridItem>
+        </Grid>
     )
 }
