9f8614ce9773110149f4a9e387ae9e62a89dda9f
nate <nate.mcdonald@att.net>
2023-01-26 23:51:03 -0600

    hero

--- src/layout/Body.js
+++ src/layout/Body.js
@@ -1,5 +1,5 @@
 
-import {Grid} from "@chakra-ui/react";
+import {Container, Grid, Box, Flex, Spacer} from "@chakra-ui/react";
 
 export function Body({children}){
     return(
@@ -7,8 +7,11 @@ export function Body({children}){
             position='fixed' w='100%' top='10vh' bot='10vh'
             h='80vh'
             style={{overflowY: "scroll", overflowX: "hidden"}}
+
         >
             {children}
+            <Box p='4' h='4.5rem'>
+            </Box>
         </Grid>
     )
 }
