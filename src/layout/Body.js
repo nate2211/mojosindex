74e5b79bde3a31125985b1fdb7abc2dd44803f12
nate <nate.mcdonald@att.net>
2023-01-26 22:57:44 -0600

    hero

--- src/layout/Body.js
+++ src/layout/Body.js
@@ -5,7 +5,7 @@ export function Body({children}){
     return(
         <Grid
             h='80vh'
-            style={{overflowY: "scroll", overflowX: "hidden", paddingBottom:"3.5rem"}}
+            style={{overflowY: "scroll", overflowX: "hidden", paddingBottom:"4.5rem"}}
         >
             {children}
         </Grid>
